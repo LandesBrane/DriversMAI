e61e71bad1f3705cffeb333a6217c2706bf675b2
LandesBrane <landesbrane@gmail.com>
2025-12-18 20:41:16 +0000

    pz8 ready

---
+++ PZ8/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/admin/DriversMAI/PZ8/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/admin/DriversMAI/PZ8/Module.symvers -T /home/admin/DriversMAI/PZ8/modules.order -i Module.symvers -e 
